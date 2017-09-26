1d74b51d5c04e458ad3c91b9f90d15db496e4f15
mkg20001 <mkg20001@gmail.com>
2017-09-26 14:25:39 +0200

    Add tests - Update

---
+++ test/listen-and-dial.spec.js
@@ -0,0 +1,144 @@
+"use strict"
+
+const addrs = ["/ip4/127.0.0.1/tcp/15001/ws/p2p-websocket-star", "/ip4/127.0.0.1/tcp/15002/ws/p2p-websocket-star", "/ip6/::1/tcp/15003/ws/p2p-websocket-star"]
+const addr_off = "/ip4/127.0.0.1/tcp/16000/ws/p2p-websocket-star"
+
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
+const multiaddr = require("multiaddr")
+const getMa = id => multiaddr("/p2p-websocket-star/ipfs/" + id.toB58String())
+const pull = require("pull-stream")
+const pullTestData = [Buffer.from("hello world!")]
+const pullTest = (conn, cb) => pull(pull.values(pullTestData.slice(0)), conn, pull.collect((err, res) => {
+  expect(err).to.not.exist()
+  expect(res).to.eql(pullTestData)
+  cb()
+}))
+chai.use(dirtyChai)
+
+const Id = require("peer-id")
+const {
+  each,
+  map
+} = require("async")
+const multi = require("..")
+
+describe("websocket-star-multi", () => {
+  let ids
+  before(cb => map(require("./ids.json"), Id.createFromJSON, (err, _) => err ? cb(err) : cb(null, (ids = _))))
+  let l = []
+  describe("listen", () => {
+    let id
+    before(() => {
+      id = ids[0]
+    })
+    it("should listen on 1 address", cb => {
+      const listener = new multi({
+        id,
+        servers: [addrs[0]]
+      }).createListener()
+      l.push(listener)
+      listener.listen(getMa(id), err => {
+        expect(err).to.not.exist()
+        expect(listener.online).to.have.lengthOf(1)
+        cb()
+      })
+    })
+
+    it("should listen on 3 addresses", cb => {
+      const listener = new multi({
+        id,
+        servers: addrs
+      }).createListener()
+      l.push(listener)
+      listener.listen(getMa(id), err => {
+        expect(err).to.not.exist()
+        expect(listener.online).to.have.lengthOf(3)
+        cb()
+      })
+    })
+
+    it("listen on offline server should fail", cb => {
+      const listener = new multi({
+        id,
+        servers: [addr_off]
+      }).createListener()
+      l.push(listener)
+      listener.once("error", () => {})
+      listener.listen(getMa(id), err => {
+        expect(err).to.exist()
+        expect(listener.online).to.have.lengthOf(0)
+        cb()
+      })
+    })
+
+    it("listen on offline server and up server should not fail", cb => {
+      const listener = new multi({
+        id,
+        servers: [addrs[0], addr_off]
+      }).createListener()
+      l.push(listener)
+      listener.listen(getMa(id), err => {
+        expect(err).to.not.exist()
+        expect(listener.online).to.have.lengthOf(1)
+        cb()
+      })
+    })
+
+    it("listen on offline server with ignore_no_online should not fail", cb => {
+      const listener = new multi({
+        id,
+        servers: [addr_off],
+        ignore_no_online: true
+      }).createListener()
+      l.push(listener)
+      listener.listen(getMa(id), err => {
+        expect(err).to.not.exist()
+        expect(listener.online).to.have.lengthOf(0)
+        cb()
+      })
+    })
+  })
+  describe("dial", () => {
+    let c1, c2, c3
+    before(cb => {
+      map(ids, (id, cb) => {
+        const m = new multi({
+          id,
+          servers: addrs
+        })
+        const l = m.createListener(conn => pull(conn, conn))
+        l.id = id
+        l.dial = m.dial.bind(m)
+        l.listen(getMa(id), err => err ? cb(err) : cb(null, l))
+      }, (err, c) => {
+        expect(err).to.not.exist()
+        c1 = c.shift()
+        c2 = c.shift()
+        c3 = c.shift()
+        cb()
+      })
+    })
+
+    it("c1 should dial to c2 over server 2", cb => {
+      const toDial = multiaddr(addrs[1] + "/ipfs/" + c2.id.toB58String())
+      c1.dial(toDial, (err, conn) => {
+        expect(err).to.not.exist()
+        pullTest(conn, cb)
+      })
+    })
+
+    it("c1 dial c3 over non-existent server should fail", cb => {
+      const toDial = multiaddr(addr_off + "/ipfs/" + c2.id.toB58String())
+      c1.dial(toDial, (err, conn) => {
+        expect(err).to.exist()
+        expect(conn).to.not.exist()
+        cb()
+      })
+    })
+
+    after(() => l.push(c1, c2, c3))
+  })
+  afterEach(cb => each(l, (l, cb) => l.close(cb), err => err ? cb(err) : cb(null, (l = []))))
+})
